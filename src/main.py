4479952463d8cbaf9e8aa1e315fa0281b9567c62
ahmetarift <turkmens74@gmail.com>
2023-08-17 13:21:38 +0300

    style: better outputs with a function

--- src/main.py
+++ src/main.py
@@ -28,6 +28,13 @@ def get_labels(parent):
     return json.loads(config['Labels']['sentiment']), json.loads(config['Labels']['intent'])
 
 
+def create_output(message, candidates):
+    full_output = classifier(message, candidates)
+    label = full_output['labels'][0]
+    probability = full_output['scores'][0]
+    return f"{label} with probability = {probability}"
+
+
 if __name__ == '__main__':
     parent = get_parent_path()
     model_path, data_path = get_model_data_path(parent)
@@ -39,19 +46,8 @@ if __name__ == '__main__':
     classifier = pipeline(task="zero-shot-classification",
                           model=model_path)
 
+    for sequence in data:
+        print(f"Sequence: {sequence['message']}")
+        print(f"Sentiment is: {create_output(sequence['message'], candidate_sentiments)}")
+        print(f"Intention is: {create_output(sequence['message'], candidate_intentions)}\n")
 
-
-    print(candidate_sentiments)
-    print(candidate_intentions)
-
-
-    sequence_to_classify = "Certainly, the iPhone 14 comes with a range of new features. Improved camera, faster processor, and longer battery life, to name a few."
-    candidate_sentiments = ['positive', 'neutral', 'negative']
-    print(classifier(sequence_to_classify, candidate_sentiments))
-    print(classifier(sequence_to_classify, candidate_sentiments)['labels'][0])
-
-    print('Intention classifier:')
-    candidate_intentions = ['Greetings and farewells', 'Discuss features of product', 'Price and discounts']
-
-    print(classifier(sequence_to_classify, candidate_intentions))
-    print(classifier(sequence_to_classify, candidate_intentions)['labels'][0])
