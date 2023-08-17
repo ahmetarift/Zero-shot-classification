96bf101b957f1132e521da93aa94bab5b0aa19cb
ahmetarift <turkmens74@gmail.com>
2023-08-17 07:55:03 +0300

    feat: Two different classifiers

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ if __name__ == '__main__':
     classifier = pipeline("zero-shot-classification",
                           model="./resources")
 
-    sequence_to_classify = "one day Bilge and I will see the world."
-    candidate_labels = ['travel', 'dancing', 'playing soccer']
-    print(classifier(sequence_to_classify, candidate_labels))
+    sequence_to_classify = "Certainly, the iPhone 14 comes with a range of new features. Improved camera, faster processor, and longer battery life, to name a few."
+    candidate_sentiments = ['positive', 'neutral', 'negative']
+    print(classifier(sequence_to_classify, candidate_sentiments))
+    print(classifier(sequence_to_classify, candidate_sentiments)['labels'][0])
 
-    print(classifier(sequence_to_classify, candidate_labels)['labels'][0])
+    print('Intention classifier:')
+    candidate_intentions = ['Greetings and small talks', 'Discuss features of product', 'Price and discounts']
 
-
-    # add tests
+    print(classifier(sequence_to_classify, candidate_intentions))
+    print(classifier(sequence_to_classify, candidate_intentions)['labels'][0])
 
 
 
