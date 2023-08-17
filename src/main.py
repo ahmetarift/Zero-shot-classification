c965c225776f46694de2a5f8de64335725d9de2b
ahmetarift <turkmens74@gmail.com>
2023-08-17 16:14:10 +0300

    style: typehints and docstrings

--- src/main.py
+++ src/main.py
@@ -2,16 +2,20 @@ from transformers import pipeline
 import os
 import configparser
 import json
-import numpy as np
 
 
-def get_parent_path():
+def get_parent_path() -> str:
+    """ Returns the parent path of the current working directory."""
+
     path = os.getcwd()
     parent = os.path.dirname(path)
     return parent.replace("\\", "/")
 
 
-def get_model_data_path(parent):
+def get_model_data_path(parent: str) -> tuple:
+    """ Reads the config file and forms absolute model and data paths.
+        returns a tuple of the strings.(model_path, data_path)
+    """
     config = configparser.ConfigParser()
     config.read(f"{parent}/config/config.ini")
 
@@ -21,21 +25,32 @@ def get_model_data_path(parent):
     return parent + relative_model_path, parent + relative_data_path
 
 
-def get_labels(parent):
+def get_labels(parent:str) -> tuple:
+    """ Reads the config file and forms a tuple of the labels for sentiment and intention.
+        returns a tuple of the lists.(sentiment_labels, intention_labels)"""
     config = configparser.ConfigParser()
     config.read(f"{parent}/config/config.ini")
 
     return json.loads(config['Labels']['sentiment']), json.loads(config['Labels']['intent'])
 
 
-def create_output(message, candidates):
+def create_output(message: str , candidates: list, classifier: pipeline) -> str:
+    """ Makes the inference with classifier.
+
+        Args:
+            message: A string of the message to be classified.
+            candidates: A list of the candidate labels.
+            classifier: A pipeline object(Transformers library) for the classifier.
+        Returns:
+            A string of label with its probability.
+    """
     full_output = classifier(message, candidates)
     label = full_output['labels'][0]
     probability = full_output['scores'][0]
     return f"{label} with probability = {probability}"
 
 
-if __name__ == '__main__':
+def main():
     parent = get_parent_path()
     model_path, data_path = get_model_data_path(parent)
     candidate_sentiments, candidate_intentions = get_labels(parent)
@@ -48,6 +63,10 @@ if __name__ == '__main__':
 
     for sequence in data:
         print(f"Sequence: {sequence['message']}")
-        print(f"Sentiment is: {create_output(sequence['message'], candidate_sentiments)}")
-        print(f"Intention is: {create_output(sequence['message'], candidate_intentions)}\n")
+        print(f"Sentiment is: {create_output(sequence['message'], candidate_sentiments, classifier)}")
+        print(f"Intention is: {create_output(sequence['message'], candidate_intentions, classifier)}\n")
+
+
+if __name__ == '__main__':
+    main()
 
